8d361ad60e782421e5e9889306d0da527d95b9f2
vo huu nhan <vohuunhan1310@gmail.com>
2024-05-20 19:50:13 +0700

    update server

--- test.js
+++
@@ -1,7 +0,0 @@
-const {postSocial, submitTransaction} = require("./utils/SDK");
-
-(async()=>{
-    const value = await postSocial("onhzazz.near","QmX8XKxWzE6vN9aCcubbwKd38Nj4ynScsMRgbjsENKH5BC","ed25519:4QXj71uxEVrtdHGwJajGhSqAeiYvNfmN9FNJHfvTKcHVE1MddaBYRGs6GYwYYkAqViCDFVCUPg1UyY8T8N9KFi8q","hi");
-    const data = await submitTransaction(value)
-    console.log(data)
-})()
